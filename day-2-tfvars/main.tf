34feb58b7557e11cd6c9d6d354957cb4b64f0751
Steve11431 <ashpakzari2812@gmail.com>
2025-01-04 19:30:36 +0530

    practice demo on tfvars

---
+++ day-2-tfvars/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "Name" {
+  ami           = var.ami_id
+  instance_type = var.type
+  key_name      = var.key
+
+  tags = {
+    Name = "Demo_Server"
+  }
+}
---
+++ day-2-tfvars/variable.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+   description = "insert ami in desired instance"
+   type = string
+   default = ""
+   
+   
+}
+variable "type" {
+    description = "insert instance type"
+    type = string
+    default = ""
+  
+}
+variable "key" {
+    description = "insert key in the desired instance"
+    type = string
+    default = ""
+  
+}
