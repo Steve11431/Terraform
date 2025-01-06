eb90c1d29a697627e029d7ac2831d8c71c7500f2
Steve11431 <ashpakzari2812@gmail.com>
2025-01-06 20:18:19 +0530

    backend file

--- day-3-statefile/main.tf
+++ day-3-statefile/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "name" {
      ami = var.ami_id
     instance_type = var.type
     key_name = var.key
+    availability_zone = "ap-south-1a"
     tags = {
         Name="Windows"
     }
