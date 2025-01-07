7cb2cc488eed21fdf8c043c071e7e2b76ec4b291
Steve11431 <ashpakzari2812@gmail.com>
2025-01-07 20:01:40 +0530

    backend config

--- day-3-dyanamodb-resource/main.tf
+++ day-3-dyanamodb-resource/main.tf
@@ -1,21 +1,21 @@
-provider "aws" {
+#creating s3 bucket and dynamo DB for state backend storgae and applying the lock mechanisam for statefile
+
+provider "aws" { 
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "demostatefilelocking" #you can chnage bucket name 
   
 }
-resource "aws_s3_bucket" "Statefile_bucket" {
-    bucket = "demopractice123"
-  
-}
-resource "aws_dynamodb_table" "dyanamodb-terraform-state-lock" {
 
-    name = "terraform-state-lock-dyanamo"
-    hash_key = "lockID"
-    read_capacity = 20
-    write_capacity = 20
-
-    attribute {
-        name = "lockID"
-        type = "S"
-      
-    }
-  
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
