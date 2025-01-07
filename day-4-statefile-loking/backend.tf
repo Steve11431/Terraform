f4e57cd8cd03aaf3b86a8858f8ba4b53955b431a
Steve11431 <ashpakzari2812@gmail.com>
2025-01-07 19:09:04 +0530

    dyanamodb-lockfile

--- day-4-statefile-loking/backend.tf
+++ day-4-statefile-loking/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket = "newdemo11431"
     key = "terraform.tfstate"
+    region = "ap-south-1"
     dynamodb_table = "terraform-state-lock-dyanamo"
     encrypt = true
     
